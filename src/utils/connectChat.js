f8e6de21de91fae7dd9e6bf29575feb3a8634d79
Kim-soung-won <rlatmddnjs0103@naver.com>
2024-07-22 14:24:09 +0900

    jwt Token URL 파라미터로 chat 서비스에 연결

--- src/utils/connectChat.js
+++ src/utils/connectChat.js
@@ -5,9 +5,14 @@ import {url} from './single.js';
 const connectChat = (e) => {
     jwtAxios.get(`${url}/api/chat?id=${e}`).then((response) => {
         console.log(response);
+        const persistData = JSON.parse(localStorage.getItem("persist:root"));
+        console.log("persistData : "+persistData)
+        const memberSlice = JSON.parse(persistData.MemberSlice);
+        console.log("memberSlice : "+memberSlice)
+        const token = memberSlice.token;
         const encoder = new TextEncoder();
         const encodedNickname = btoa(encoder.encode(response.data.nickname).reduce((data, byte) => data + String.fromCharCode(byte), ''));
-        const redirectUrl = `http://175.45.204.119:3000/chat/${response.data.roomId}-${encodedNickname}`;
+        const redirectUrl = `http://175.45.204.119:3000/chat/${response.data.roomId}-${encodedNickname}?token=${token}`;
         window.location.href = redirectUrl;
     }).catch((error) => {
         console.error(error);
